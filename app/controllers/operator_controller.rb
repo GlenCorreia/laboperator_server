114b53325044c1f5155655774703c2fa259a5dbd
Glen Correia <glen_correia@outlook.com>
2021-12-12 17:27:08 +0530

    Clean the input data and return appropriate response

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -1,7 +1,7 @@
 class OperatorController < ApplicationController
 
   def clean
-    data = params[:data] || nil
+    data      = params[:data] || nil
     threshold = params[:threshold] || nil
 
     if data.nil? || threshold.nil?
@@ -9,5 +9,18 @@ class OperatorController < ApplicationController
         message: "Please enter appropriate parameters"
       }, status: :bad_request
     end
+
+    output_list = []
+    data.each do |item|
+      if item < threshold
+        output_list << 0
+      else 
+        output_list << 1
+      end
+    end
+
+    return render json: {
+      signal: output_list
+    }
   end
 end
