96bdb649af2b46a1bfdfe374128ffffc0937917b
Glen Correia <glen_correia@outlook.com>
2021-12-12 18:37:14 +0530

    Create device_signals table and migrate up the DB

---
+++ db/migrate/20211212125833_create_device_signal.rb
@@ -0,0 +1,11 @@
+class CreateDeviceSignal < ActiveRecord::Migration[6.1]
+  def change
+    create_table :device_signals do |t|
+      t.integer :noise
+      t.integer :signal
+      t.integer :threshold
+      
+      t.timestamps
+    end
+  end
+end
