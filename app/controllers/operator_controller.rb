b5029e63fbe13c77b2c4fdbf6e32be609cd7281d
Glen Correia <glen_correia@outlook.com>
2021-12-12 17:16:26 +0530

    Define clean action in OperatorController

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -1,2 +1,13 @@
 class OperatorController < ApplicationController
+
+  def clean
+    data = params[:data] || nil
+    threshold = params[:threshold] || nil
+
+    if data.nil? || threshold.nil?
+      return render json: {
+        message: "Please enter appropriate parameters"
+      }, status: :bad_request
+    end
+  end
 end
