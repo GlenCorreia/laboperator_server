daf2970b97566a21974ba9291a7248e92f557325
Glen Correia <glen_correia@outlook.com>
2021-12-12 18:51:13 +0530

    Redo migration

--- db/migrate/20211212130741_create_device_signals.rb
+++ db/migrate/20211212130741_create_device_signals.rb
@@ -1,7 +1,10 @@
 class CreateDeviceSignals < ActiveRecord::Migration[6.1]
   def change
     create_table :device_signals do |t|
-
+      t.integer :noise
+      t.integer :signal
+      t.integer :threshold
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_125833) do
+ActiveRecord::Schema.define(version: 2021_12_12_130741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
